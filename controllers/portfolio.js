4d2541fd00d4cac1f46d7dc8df22089ad004ab10
Adam Estela <adam.estela@paperg.com>
2014-07-30 21:04:01 -0700

    popups on hover

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,5 +1,8 @@
 function PortfolioController($scope){
 	var $container = $('.portfolio-items');
+	$container.isotope();
+
+	var itemHover = false;
 
 	$scope.filterPortfolio = function(filterValue){
 		$container.isotope({filter:filterValue});
