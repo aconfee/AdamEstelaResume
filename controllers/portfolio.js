bcc66921fd33a684cfba90e2670cb7276468cf91
Adam Estela <adamestela@gmail.com>
2014-07-31 00:57:01 -0700

    fancy portfolio

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -2,7 +2,19 @@ function PortfolioController($scope){
 	var $container = $('.portfolio-items');
 	$container.isotope();
 
-	var itemHover = false;
+	$scope.hovered = -1;
+
+	$scope.showTabs = function(index){
+		var $element = $('.portfolio-tabs').eq(index);
+		$element.animate({height: "40px"}, 200)
+		$scope.hovered = index;
+	};
+
+	$scope.hideTabs = function(index){
+		var $element = $('.portfolio-tabs').eq(index);
+		$element.animate({height:"0px"}, 200);
+		$scope.hovered = -1;
+	};
 
 	$scope.filterPortfolio = function(filterValue){
 		$container.isotope({filter:filterValue});
