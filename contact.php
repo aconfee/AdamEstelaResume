b5e6d2237ba5b67e2006e0bf5d53908b6bf6ef05
Adam Estela <adamestela@gmail.com>
2014-07-31 16:27:42 -0700

    debug

--- contact.php
+++ contact.php
@@ -2,12 +2,16 @@
     echo '<p>doing stuff...</p>';
 
     $name = $_POST['name'];
+    echo "$name";
     $email = $_POST['email'];
+    echo "$name";
     $message = $_POST['message'];
+    echo "$name";
     $from = 'From: Resume'; 
     $to = 'adamestela@gamil.com'; 
     $subject = 'Resume Inquery';
     //$human = $_POST['human'];
+    echo "done doing stuff";
 			
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 				
