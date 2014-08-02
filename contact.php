1cc809254c7b6d4886636bcd8c1c670592bd368d
Adam Estela <adam.estela@paperg.com>
2014-08-01 18:22:02 -0700

    contact fixes

--- contact.php
+++ contact.php
@@ -1,15 +1,19 @@
 <?php
     echo "doing stuff";
+    print_r(error_get_last());
 
     // Pear Mail Library
     require_once "Mail.php";
 
+    print_r(error_get_last());
     echo "included mailphp";
 
     $from = '<adamestela.gmail.com>';
     $to = '<icyflame198.yahoo.com>';
 
-    echo "to $to";
+    print_r(error_get_last());
+    echo "to $to";  
+
     $subject = 'Hi!';
     $body = "Hi,\n\nHow are you... DOUCHEBAGGG?";
 
@@ -19,6 +23,7 @@
         'Subject' => $subject
     );
 
+    print_r(error_get_last());
     echo "array made";
 
     $smtp = Mail::factory('smtp', array(
@@ -29,13 +34,16 @@
             'password' => 'elcoqui18'
         ));
 
+    print_r(error_get_last());
     echo "smtp made, sendint message";
 
     $mail = $smtp->send($to, $headers, $body);
 
+    print_r(error_get_last());
     echo "mesage sent. mail: $mail";
 
     if (PEAR::isError($mail)) {
+        print_r(error_get_last());
         echo('<p>' . $mail->getMessage() . '</p>');
     } else {
         echo('<p>Message successfully sent!</p>');
