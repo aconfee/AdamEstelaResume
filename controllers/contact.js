7a6c0a37277addd3eba03a33618fb69cc4cad283
Adam Estela <adamestela@gmail.com>
2014-08-04 01:20:26 -0700

    mail delivery complete

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,32 +1,36 @@
 function ContactController($scope){
 
   $scope.sendEmail = function(){
+    // Get email info
+    var $name = $('input[name="name"]');
+    var $email = $('input[name="email"]');
+    var $message = $('textarea[name="message"]');
+
     $.ajax({
       type: 'POST',
       url: 'https://mandrillapp.com/api/1.0/messages/send.json',
       data: {
         'key': 'YCIdHt4plO84mUGeIOM11Q',
         'message': {
-          'from_email': 'adamestela@gmail.com',
+          'from_email': $email.val(),
           'to': [
               {
-                'email': 'aconfee@charter.net',
-                'name': 'Pigly',
+                'email': 'adamestela@gmail.com',
+                'name': $name.val(),
                 'type': 'to'
               },
-              {
-                'email': 'adam.estela@paperg.com',
-                'name': 'Pigly2',
-                'type': 'to'
-              }
             ],
           'autotext': 'true',
-          'subject': 'Resume Inquery',
-          'html': 'Finally an email is sent!'
+          'subject': 'Resume Inquery [From my site]',
+          'html': $message.val()
         }
       }
      }).done(function(response) {
-       console.log(response); // if you're into that sorta thing
+       console.log(response);
      });
+
+     $name.val('');
+     $email.val('');
+     $message.val('');
   }
 }
