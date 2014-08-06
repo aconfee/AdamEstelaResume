6b5cc4fd462431305e5e0d1d7c8dc8477cc64a78
Adam Estela <adamestela@gmail.com>
2014-08-06 03:03:02 -0700

    final polish. animations

--- controllers/fades.js
+++ controllers/fades.js
@@ -1,6 +1,5 @@
 $(window).scroll(function(){
      var top = $(this).scrollTop();
-     console.log(top);
      if(top > 600){
        	$("#gamesOverview").animate({
        		left:'0px',
@@ -17,23 +16,47 @@ $(window).scroll(function(){
        	$("#testOverview").animate({
        		left:'0px',
      		opacity:1
-     	}, 1600);
+     	}, 1400);
 
      	$("#testOverviewIcon").animate({
        		top:'0px',
      		opacity:1
-     	}, 1600);
+     	}, 1400);
      }
 
      if(top > 850){
        	$("#webOverview").animate({
        		left:'0px',
      		opacity:1
-     	}, 2000);
+     	}, 1600);
 
      	$("#webOverviewIcon").animate({
        		top:'0px',
      		opacity:1
-     	}, 2000);
+     	}, 1600);
+     }
+
+     if(top > 2550){
+       	$("#education").animate({
+       		top:'0px',
+     		opacity:1
+     	}, 1000);
+
+     	$("#educationText").animate({
+       		left:'0px',
+     		opacity:1
+     	}, 1000);
+     }
+
+     if(top > 3050){
+       	$("#paperg").animate({
+       		right:'0px',
+     		opacity:1
+     	}, 1200);
+
+     	$("#digipen").animate({
+       		left:'0px',
+     		opacity:1
+     	}, 1200);
      }
 });
